5241c026ad614849404eaebb29f0c3e2228fbf84
Ryzen XP <iron4540@gmail.com>
2025-05-19 13:16:02 +0530

    fix error

--- smart-contract/web3.js
+++ smart-contract/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // we are on the server *OR* meta mask is not running
   // creating our own provider
   const provider = new Web3.providers.HttpProvider(
-    "https://holesky.infura.io/v3/21266fb86e1443869b2bd28f2180a25c"
+    "https://ethereum-holesky-rpc.publicnode.com"
   );
 
   web3 = new Web3(provider);
